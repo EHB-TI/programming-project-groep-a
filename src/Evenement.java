c3a301328508235a179ea4b1a0f905513dd67dd6
regehb <reg.carremans@student.ehb.be>
2022-05-01 15:10:35 +0200

    Added to do's to class Evenement.

--- src/Evenement.java
+++ src/Evenement.java
@@ -7,10 +7,10 @@ public class Evenement {
     private String title;
     private Date startDate;
     private Date endDate;
-    //TO DO brouwerij
-    //TO DO bier
-    //TO DO streek
-    //TO DO bezoekers []
+    //TO DO organising brewery
+    //TO DO featured beer
+    //TO DO region (or is this based on featured beer?)
+    //TO DO visitors []
 
     //constructors
     public Evenement() {
@@ -52,4 +52,22 @@ public class Evenement {
     }
 
     //methods
+    //TO DO add visitor to event
+        //TO DO add visitor to visitors arrayList
+        //TO DO check if user already in visitors arrayList. If so, throw an exception
+        //TO DO check that event's start date hasn't passed yet. If so, don't add visitors
+
+    //TO DO delete visitor from event
+
+    //TO DO add featured beer to event. Maximum of one.
+
+    //TO DO delete featured beer from event
+
+    //TO DO add an organising brewery to event. Maximum of one.
+
+    //TO DO delete an organising brewery from event
+
+    //TO DO print the event's details plus a list of visitors
+
+//ending brackets (because I get confused sometimes)
 }
