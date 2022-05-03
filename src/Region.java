b771bdbffaae4df2ef8264e4a4fad527fe563828
Tom Oostvogels <tom.oostvogels@student.ehb.be>
2022-05-03 11:56:26 +0200

    Aanmaak klassen

---
+++ src/Region.java
@@ -0,0 +1,44 @@
+public class Region {
+    //members
+    private static int regionCounter = 0;
+    private final int regionID;
+    private String name, country;
+    // Nog dingen?
+
+    //constructors
+    public Region(String name, String country) {
+        this.name = name;
+        this.country = country;
+        this.regionID = regionCounter++;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getRegionID() {
+        return regionID;
+    }
+
+    public static int getRegionCounter() {
+        return regionCounter;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    // Methods
+    // TO DO delete regions, how to deal with region counter and region ID's?
+
+}
---
+++ src/User.java
@@ -0,0 +1,81 @@
+import java.util.Date;
+
+public class User {
+    //members
+    private static int userCounter = 0;
+    private final int userID;
+    private String name, surname;
+    private int age;
+    // Automatisch huidige datum als waarde toevoegen?
+    // private Date joiningDate;
+    private int favoriteBeer;
+
+    // Lijst gedronken bieren
+    // Lijst bezochte plaatsen/streken
+    // Nog dingen?
+
+    //constructors
+    public User(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.userID = userCounter; // Klopt dit?
+        userCounter++;
+    }
+
+    public User(String name, String surname, int age, int favoriteBeer) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.favoriteBeer = favoriteBeer;
+        this.userID = userCounter; // Klopt dit?
+        userCounter++;
+    }
+
+    // Getters
+    public int getUserID() {
+        return userID;
+    }
+
+    public static int getUserCounter() {
+        return userCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getFavoriteBeer() {
+        return favoriteBeer;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFavoriteBeer(int favoriteBeer) {
+        this.favoriteBeer = favoriteBeer;
+    }
+
+    // Methods
+    // TO DO add beer to beer list
+    // TO DO add locations to location list
+    // TO DO delete user, how to deal with user counter and user ID's?
+}
