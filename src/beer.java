9bd4756d88e430bf67b60d7165db3381075884dd
polar <polar.vaes@student.ehb.be>
2022-05-05 15:40:52 +0200

    added class beer

---
+++ src/beer.java
@@ -0,0 +1,58 @@
+public class beer {
+    // members
+    private static int beerIDCounter = 0;
+    private final int beerID;
+    private String name;
+    private  int alcoholPercentage,ibu, rating;
+    // wat nog toe te voegen?
+    // constructors
+
+    public beer(String name, int alcoholPercentage) {
+        this.name = name;
+        this.alcoholPercentage = alcoholPercentage;
+        this.beerID = ++beerIDCounter;
+    }
+
+    public beer(String name, int alcoholPercentage, int ibu, int rating) {
+        this.name = name;
+        this.alcoholPercentage = alcoholPercentage;
+        this.ibu = ibu;
+        this.rating = rating;
+        this.beerID = ++beerIDCounter;
+    }
+    // linken nog aan brouwerij?
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAlcoholPercentage() {
+        return alcoholPercentage;
+    }
+
+    public int getIbu() {
+        return ibu;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAlcoholPercentage(int alcoholPercentage) {
+        this.alcoholPercentage = alcoholPercentage;
+    }
+
+    public void setIbu(int ibu) {
+        this.ibu = ibu;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    // methods
+}
