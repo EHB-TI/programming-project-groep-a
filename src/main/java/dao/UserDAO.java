94a17aca466fbc3e2fdffe49ad77aff2d929ad22
Tom Oostvogels <tom.oostvogels@student.ehb.be>
2022-05-09 16:53:11 +0200

    Timestamp voor aanmaak van gebruiker in database staat 2 uur vroeger, proberen oplossen maar nog niet gelukt.

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 
 import entity.User;
 
+import javax.management.StandardEmitterMBean;
+
 public class UserDAO extends BaseDAO{
     public void saveUser(User u)
     {
@@ -22,9 +24,15 @@ public class UserDAO extends BaseDAO{
     public void saveUser()
     {
         try(Connection conn = getConn()){
-            Statement s = conn.createStatement();
             // Dit moet nog aangepast worden om ingelezen waardes van klasse 'User' te gebruiken in plaats van vooringevulde zaken
-            s.executeUpdate("INSERT INTO `gebruikers`(`gebruikersid`, `voornaam`, `achternaam`, `leeftijd`, `geslacht`, `beroep`, `woonplaats`, `email`, `toetreding`) VALUES (NULL,'Timo','Koninckx',45,'M','Bankier','Antwerpen','timotimo@gmail.com',NULL)");
+            // De tijdzone van de database server staat 2 uur vroeger, wordt nu aangepast via telkens een SQL statement
+            // Dit moet ook telkens opnieuw per sessie
+            // Misschien vragen of dit voor de server permanent kan gebeuren?
+            /* Dit werkt niet...
+            Statement timeUpdate = conn.createStatement();
+            timeUpdate.executeUpdate("SET time_zone = '+02:00';");*/
+            Statement s = conn.createStatement();
+            s.executeUpdate("INSERT INTO `gebruikers`(`gebruikersid`, `voornaam`, `achternaam`, `leeftijd`, `geslacht`, `beroep`, `woonplaats`, `email`, `toetreding`) VALUES (NULL,'Benjamin','Dewilde',65,'M','Historicus','Merchtem','bdw@gmail.com',NULL)");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -42,8 +50,8 @@ public class UserDAO extends BaseDAO{
     public static void main(String[] args) {
         // NIET UITVOEREN TENZIJ JE DUMMY WAARDES HIERBOVEN AANPAST
         // Anders zit er een dubbele gebruiker in databank, op zich niet heel erg ma bon
-        /*UserDAO udao = new UserDAO();
-        udao.saveUser();*/
+        UserDAO udao = new UserDAO();
+        udao.saveUser();
     }
 
 }
